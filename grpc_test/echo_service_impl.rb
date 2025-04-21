6d8b422b793a96bbec1fcd3982eb61a7625ebb29
Wouter Coppieters <wc@pico.net.nz>
2025-04-21 20:10:19 +1200

    Removed outdated dirs

--- grpc_test/echo_service_impl.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'echo_services_pb'
-
-class EchoServiceImpl < Echo::EchoService::Service
-  # Unary RPC implementation
-  def echo(req, _unused_call)
-    Echo::EchoResponse.new(message: req.message)
-  end
-end
